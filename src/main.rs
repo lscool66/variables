0ab638d261f1821178ae2c5976bf97fe7393e13e
lscool66 <lscool66@outlook.com>
2022-07-23 09:43:40 +0800

    variables

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+// const MAX_POINTS: u32 = 100_000;
+fn main() {
+    // const MAX_POINTS: u32 = 100_000;
+
+    // println!("Hello, world!");
+
+    // let x = 5;
+    // let x = x + 1;
+    // let x = x * 2;
+    // println!("The value of x is: {}", x);
+
+    // let spaces = "      ";
+    // let spaces = spaces.len();
+    // println!("The value of spaces is: {}", spaces);
+
+    // let x = 'z';
+    // let y = 'Z';
+    // let z = '😂';
+    // println!("z的值是😂😂😂😂😂2022年07时111223１１１２２２2333，，，，,,,,.....，，，。。。,.,,..时间事实上  广告公司 桑桑贼哈能一sang间wodenidewtf月17日十二时十二分: {}", x);
+
+    // let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // let (x, y, z) = tup;
+    // println!("{}, {}, {}", x, y, z);
+    // println!("{}, {}, {}", tup.0, tup.1, tup.2);
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let index = 11;
+    println!("{}", months[index]);
+    // let a = [3; 5];
+}
